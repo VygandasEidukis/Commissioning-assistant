3e8be2a39439c342064d65a089be575a14ed1dfe
vygandaseidukis <vygandaseidukis@gmail.com>
2019-12-27 21:30:11 +0200

    base commit

---
+++ commissioning assistance/commissioning assistance/Bootstrapper.cs
@@ -0,0 +1,28 @@
+﻿using Caliburn.Micro;
+using commissioning_assistance.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace commissioning_assistance
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        #region Constructor
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+        #endregion
+
+        #region Overrides
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<MainViewModel>();
+        }
+        #endregion
+    }
+}
